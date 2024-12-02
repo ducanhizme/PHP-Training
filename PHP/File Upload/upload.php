05bee03aa0eff278129c102adb8583b4c25e739e
NinhDucAnh <94469719+ducanhizme@users.noreply.github.com>
2024-12-02 17:45:26 +0700

    Enum

---
+++ PHP/File Upload/upload.php
@@ -0,0 +1,45 @@
+<?php
+$target_dir = "sample/"; // target folder to save
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]); //target file to save
+$uploadOk = 1; //check validation upload
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));// get file type
+//check is the file is image
+if(isset($_POST["submit"])) {
+    //get file in temp dir that config in php.ini file
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
