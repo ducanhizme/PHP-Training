177fc4e6f05270d3405a56f764cbb702a3c23f6b
NinhDucAnh <94469719+ducanhizme@users.noreply.github.com>
2024-12-02 14:23:44 +0700

    Magic Method

---
+++ PHP/Magic Method/app.php
@@ -0,0 +1,40 @@
+<?php
+class HtmlElement{
+    public array $attribute =[];
+
+
+    public function __construct(public string $tag)
+    {
+    }
+//sẻet
+    public function __set(string $name, $value): void
+    {
+        $this->attribute[$name] = $value;
+    }
+//get
+    public function __get(string $name)
+    {
+        if(array_key_exists($name,$this->attribute)){
+            return $this->attribute[$name];
+        }
+    }
+
+    public function html(string $content):string
+    {
+        $html = "<$this->tag";
+        foreach ($this->attribute as $key  => $item) {
+            $html .= ' ' . $key . '="' . $item . '"';
+        }
+        $html .= '>';
+        $html .= $content;
+        $html .= "</$this->tag>";
+        return $html;
+    }
+}
+
+$div = new HtmlElement('div');
+//non - exits property
+$div->id = 'test';
+$div->class = 'dflex';
+//var_dump($div->html('Hello'));
+var_dump($div->id);
